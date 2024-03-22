236c877317b3c8e03474f5be24f8c58e9b56927d
Jared Talbot <jared.talbot-2@selu.edu>
2024-03-22 17:00:46 -0500

    more season stuff

--- Seasons.cs
+++ Seasons.cs
@@ -1,5 +1,5 @@
 using Godot;
-using System;
+using Godot.Collections;
 
 public partial class Seasons : Node
 {
@@ -7,14 +7,19 @@ public partial class Seasons : Node
 	public string Fall { get; } = "fall";
 	public string Winter { get; } = "winter";
 	public string Spring { get; } = "spring";
+	
+	public Array<string> SeasonsQueue = new();
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		SeasonsQueue.AddRange(new[] {Summer, Fall, Winter, Spring});
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 	}
+
+	public string GetCurrentSeason() => SeasonsQueue[0];
 }
